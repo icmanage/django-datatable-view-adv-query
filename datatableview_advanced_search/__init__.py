ee388d7e79aa3049ad359a889cd5d44edf151142
Steven Klass <rh0dium@users.noreply.github.com>
2018-03-01 09:39:08 -0700

    Added in the complier

--- datatableview_advanced_search/__init__.py
+++ datatableview_advanced_search/__init__.py
@@ -4,6 +4,9 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import lexer
+import parser
+
 __author__ = 'Steven Klass'
 __version_info__ = (0, 0, 0)
 __version__ = '.'.join(map(str, __version_info__))
@@ -11,3 +14,9 @@ __date__ = '3/1/18 9:22 AM'
 __copyright__ = 'Copyright 2018 IC Manage. All rights reserved.'
 __credits__ = ['Steven Klass', ]
 __license__ = 'See the file LICENSE.txt for licensing information.'
+
+
+def compiler(expression, debug=False, log=None):
+    adv_lexer = lexer.adv_search_lex(module=lexer, debug=debug, debuglog=log)
+    adv_parser = parser.adv_search_yacc(module=parser, debug=debug, debuglog=log)
+    return adv_parser.parse(expression, lexer=adv_lexer)
