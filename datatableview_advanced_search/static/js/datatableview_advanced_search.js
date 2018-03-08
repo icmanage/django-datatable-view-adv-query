ab324a38b71d2e6176667963604d71a651ebcf14
Steven Klass <rh0dium@users.noreply.github.com>
2018-03-08 13:49:29 -0700

    This is barely functioning.

--- datatableview_advanced_search/static/js/datatableview_advanced_search.js
+++ datatableview_advanced_search/static/js/datatableview_advanced_search.js
@@ -1,18 +1,15 @@
 // datatableview_advanced_search.js
 require(['require', 'domReady'], function (require, domReady) {
     domReady(function () {
-        require(['jquery', 'datatableview'], function ($, datatableview, datatables) {
+        require(['jquery', 'datatableview'], function ($, datatableview) {
             $(document).ready(function () {
                 var search_inputs = $('.dataTables_filter input');
                 button = '<span>&nbsp;&nbsp;&nbsp;<a class="advanced_search_help_lnk" data-target="#advanced_search_help" data-dismiss="modal"><i class="fa fa-question"></i></a></span>'
                 search_inputs.each(function () {
-                    var parent = $(this).parent();
-                    $(button).appendTo(parent);
+                    $(button).appendTo($(this).parent());
                 });
                 $(".advanced_search_help_lnk").on("click", function (e) {
-                    console.log("click!!");
                     $('#advanced_search_help').modal('show');
-
                 });
 
             });
