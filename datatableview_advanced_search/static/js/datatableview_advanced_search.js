4a17544bb41f3343f9a6a6e1e088b90d6e503bc6
Steven Klass <rh0dium@users.noreply.github.com>
2018-03-09 07:55:12 -0700

    Clean that up..

--- datatableview_advanced_search/static/js/datatableview_advanced_search.js
+++ datatableview_advanced_search/static/js/datatableview_advanced_search.js
@@ -1,15 +1,18 @@
 // datatableview_advanced_search.js
-require(['require', 'domReady'], function (require, domReady) {
+/*global require */
+/*jslint this */
+require(["require", "domReady"], function (require, domReady) {
+    "use strict";
     domReady(function () {
-        require(['jquery', 'datatableview'], function ($, datatableview) {
+        require(["jquery", "datatableview"], function ($) {
             $(document).ready(function () {
-                var search_inputs = $('.dataTables_filter input');
-                button = '<span>&nbsp;&nbsp;&nbsp;<a class="advanced_search_help_lnk" data-target="#advanced_search_help" data-dismiss="modal"><i class="fa fa-question"></i></a></span>'
+                var search_inputs = $(".dataTables_filter input");
+                var button = "<span>&nbsp;&nbsp;&nbsp;<a class=\"advanced_search_help_lnk\" data-target=\"#advanced_search_help\" data-dismiss=\"modal\"><i class=\"fa fa-question\"></i></a></span>";
                 search_inputs.each(function () {
                     $(button).appendTo($(this).parent());
                 });
-                $(".advanced_search_help_lnk").on("click", function (e) {
-                    $('#advanced_search_help').modal('show');
+                $(".advanced_search_help_lnk").on("click", function () {
+                    $("#advanced_search_help").modal("show");
                 });
 
             });
